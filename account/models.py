bf19cfa0fb8af5ff362c7f6999c5e00418564991
codelieche <codelieche@gmail.com>
2017-02-09 10:53:10 +0800

    account添加Model: EmailVerifyRecord, Banner

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 # _*_ coding:utf-8 _*_
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -30,3 +31,33 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+@python_2_unicode_compatible
+class EmailVerifyRecord(models.Model):
+    '''邮箱验证Model'''
+    EMAIL_SEND_TYPE = (
+        ('register', '注册'),
+        ('forget', '找回密码')
+    )
+    code = models.CharField(max_length=20, verbose_name='验证码')
+    email = models.EmailField(max_length=50, verbose_name="邮箱")
+    send_type = models.CharField(max_length=10, choices=EMAIL_SEND_TYPE)
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
+
+class Banner(models.Model):
+    '''首页轮播图Model'''
+    title = models.CharField(max_length=100, verbose_name="标题")
+    image = models.ImageField(upload_to='banner/%Y/%M', verbose_name="轮播图",
+                              max_length=100)
+    url = models.URLField(max_length=200, verbose_name='访问地址')
+    index = models.IntegerField(default=100, verbose_name="顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "轮播图"
+        verbose_name_plural = verbose_name
+
