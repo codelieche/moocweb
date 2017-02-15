74c03666deb2775a4ae51cd13436a38a05c990fa
codelieche <codelieche@gmail.com>
2017-02-15 18:20:43 +0800

    org_list分页器Paginator

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,6 +1,7 @@
 # _*_ coding:utf-8 _*_
 from django.shortcuts import render
 from django.views.generic import View
+from django.core.paginator import Paginator
 
 from .models import CourseOrg, CityDict
 # Create your views here.
@@ -11,11 +12,19 @@ class OrgListView(View):
     def get(self, request):
         # 先查找到所有课程机构 和 城市
         all_orgs = CourseOrg.objects.all()
+
         org_nums = all_orgs.count()
         all_citys = CityDict.objects.all()
+
+        # 分页器
+        page_num = request.GET.get('page', 1)
+        p = Paginator(all_orgs, per_page=2)
+        orgs = p.page(page_num)
+
         return render(request, 'org_list.html',{
-            "all_orgs": all_orgs,
+            "all_orgs": orgs,
             "all_citys": all_citys,
-            "org_nums": org_nums
+            "org_nums": org_nums,
+            "page_num_list": range(1, p.num_pages+1),
         })
 
